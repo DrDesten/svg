2d60586c3675b0eb9f39a1b507e42129b01f0fd4
DrDesten <>
2022-12-11 19:07:31 +0100

    stuf2

--- bezier/bezier.js
+++ bezier/bezier.js
@@ -1,14 +1,22 @@
 const globalSVG = new SVG( "#canvas" )
 
+let swirl = new SVG.path().mode("bezier")
+    .point(0,0)
+    .point(40,80)
+    .point(100,20)
+    .point(30,20)
+    .point(50,60)
+    .point(50,40)
+    //.update()
+
+let square = new SVG.path().mode("bezier")
+    .point(20,80)
+    .point(80,80)
+    .point(80,20)
+    .point(20,20)
+    //.close()
+    .update()
 
 globalSVG.add(
-    new SVG.path()
-        .mode("bezier")
-        .point(0,0)
-        .point(40,80)
-        .point(100,20)
-        .point(30,20)
-        .point(50,60)
-        .point(50,40)
-        .update()
+    square
 )
